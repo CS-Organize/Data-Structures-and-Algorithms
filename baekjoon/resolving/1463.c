4f00bc2533a6a79e3b14df286b69ba8bf8272d73
seungwonme <seungwonan.me@gmail.com>
2023-03-25 22:43:21 +0900

    2023-03-25

--- baekjoon/resolving/1463.c
+++ baekjoon/resolving/1463.c
@@ -2,31 +2,31 @@
 
 int main(void)
 {
-  int x, cnt = 0;
-  scanf("%d", &x);
-  while (x != 1)
-  {
-    if (x % 3 == 0)
-    {
-      x /= 3;
-      cnt++;
-    }
-    else if ((x-1)%3==0)
-    {
-      x--;
-      cnt ++;
-    }
-    else if (x % 2 == 0)
-    {
-      x /= 2;
-      cnt++;
-    }
-    else
-    {
-      x--;
-      cnt++;
-    }
-  }
-  printf("%d\n",cnt);
-  return 0;
+	int x, cnt = 0;
+	scanf("%d", &x);
+	while (x != 1)
+	{
+		if (x % 3 == 0)
+		{
+			x /= 3;
+			cnt++;
+		}
+		else if ((x-1)%3==0)
+		{
+			x--;
+			cnt ++;
+		}
+		else if (x % 2 == 0)
+		{
+			x /= 2;
+			cnt++;
+		}
+		else
+		{
+			x--;
+			cnt++;
+		}
+	}
+	printf("%d\n",cnt);
+	return 0;
 }
