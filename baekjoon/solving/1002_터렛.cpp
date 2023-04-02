6a7c642ab44769f2c3194f43f3f5d328e7a3a413
Seungwon An <seunan@c2r3s2.42seoul.kr>
2023-04-02 19:26:34 +0900

    a

--- baekjoon/solving/1002_터렛.cpp
+++ baekjoon/solving/1002_터렛.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <cstring>
 using namespace std;
 
@@ -31,6 +31,38 @@ int main()
 			cout << cnt << '\n';
 	}
 	return (0);
-}
+} */
 
 // https://dundung.tistory.com/152
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	long long t;
+
+	cin >> t;
+	for (long long i = 0; i < t; i++)
+	{
+		long long x1, y1, r1, x2, y2, r2, cnt = 0;
+
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+		long long n = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+
+		if (n == 0 && r1 == r2)
+			cout << -1 << '\n';
+		else if (n == 0 && r1 != r2)
+			cout << 0 << '\n';
+		else if (n > (r1 + r2) * (r1 + r2))
+			cout << 0 << '\n';
+		else if (n < (r1 + r2) * (r1 + r2))
+			cout << 2 << '\n';
+		else if (n == (r1 + r2) * (r1 + r2))
+			cout << 1 << '\n';
+		// 내접도 고려해줄 것 !!
+	}
+	return (0);
+}
+
