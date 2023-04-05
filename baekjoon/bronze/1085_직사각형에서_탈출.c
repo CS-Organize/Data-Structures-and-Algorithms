936bac8c8f67855972301dae7e3b3353822fb653
seungwonme <seungwonan.me@gmail.com>
2023-04-06 01:26:48 +0900

    2023-04-06

--- baekjoon/bronze/1085_직사각형에서_탈출.c
+++ baekjoon/bronze/1085_직사각형에서_탈출.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
 	int x, y, w, h, min;
 	scanf("%d %d %d %d", &x, &y, &w, &h);
--- baekjoon/bronze/1110.c
+++ baekjoon/bronze/1110.c
@@ -2,21 +2,21 @@
 
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-  int a,b,c,d=1,result,input,cnt=0;
-  scanf("%d",&input);
-  result = input;
+	int a, b, c, d = 1, result, input, cnt = 0;
+	scanf("%d", &input);
+	result = input;
 
-  while (d!=result)
-  {
-    a=input/10;
-    b=input%10;
-    c=(a+b)%10;
-    d=(b*10)+c;
-    input=d;
-    cnt ++;
-  }
-  printf("%d",cnt);
-  return 0;
+	while (d != result)
+	{
+		a = input / 10;
+		b = input % 10;
+		c = (a + b) % 10;
+		d = (b * 10) + c;
+		input = d;
+		cnt++;
+	}
+	printf("%d", cnt);
+	return (0);
 }
