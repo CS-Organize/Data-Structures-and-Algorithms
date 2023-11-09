e6de39651619fa983d9443da4d50a1a575f6f7a2
seungwonme <seungwonan.me@gmail.com>
2023-11-09 13:06:21 +0900

    real finished 0x03

--- baekjoon/array/3273/3273.cpp
+++ baekjoon/array/3273/3273.cpp
@@ -2,24 +2,29 @@
 #include <algorithm>
 using namespace std;
 
+#define K 1000000
+
 int	main()
 {
 	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
 	int n; cin >> n;
-	int arr[n], x, cnt = 0;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	cin >> x;
+	int occur[K + 1] = { 0 };
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[i] + arr[j] == x)
-				++cnt;
-		}
+		int num; cin >> num;
+		++occur[num];
+	}
+
+	int cnt = 0, x; cin >> x;
+
+	for (int i = 0; i < (x + 1) / 2; i++)
+	{
+		if (x - i <= K && occur[i] && occur[x - i] && i != x - i)
+			++cnt;
 	}
 	cout << cnt << '\n';
+
 	return 0;
 }
