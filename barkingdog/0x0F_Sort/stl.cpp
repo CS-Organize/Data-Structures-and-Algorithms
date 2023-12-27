32c755206c97c830062411e561a83120766039af
seungwonme <seungwonan.me@gmail.com>
2023-12-28 01:34:04 +0900

    Finish 0x0F

--- barkingdog/0x0F_Sort/stl.cpp
+++ barkingdog/0x0F_Sort/stl.cpp
@@ -2,6 +2,13 @@
 #include <algorithm>
 using namespace std;
 
+// a가 b의 앞에 와야할 때 true, 그렇지 않을 때 false
+// 주의: 두 값이 같을 때 false를 반환하지 않으면 run time error 발생
+bool	cmp(int a, int b)
+{
+	return a < b;
+}
+
 int	main(void)
 {
 	int a[5] = {1, 4, 5, 2, 7};
@@ -9,6 +16,7 @@ int	main(void)
 
 	vector<int> b = {1, 4, 5, 2, 7};
 	sort(b.begin(), b.end());
+	// sort(b.begin(), b.begin() + 5);
 
 	for (int i = 0; i < 5; i++)
 		cout << a[i] << ' ';
@@ -18,5 +26,12 @@ int	main(void)
 		cout << b[i] << ' ';
 	cout << '\n';
 	
+	int c[7] = {7, 6, 5, 4, 3, 2, 1};
+	sort(c, c + 7, cmp);
+
+	for (int i = 0; i < 7; i++)
+			cout << c[i] << ' ';
+		cout << '\n';
+
 	return 0;
 }
